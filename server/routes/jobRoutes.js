88fcde6b729ec06d43335f631d4a8dc770374793
Dhanush C D <152018990+Dhanushcdivakar@users.noreply.github.com>
2025-08-05 09:51:37 +0530

    Update jobRoutes.js

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -11,5 +11,6 @@ router.get('/jobcards', getJobCards);
 router.post('/jobs/start', startJob);
 router.post('/jobs/stop', stopJob);
 router.get('/jobs/completed', getCompletedJobs);
+router.get('/jobs/active', getActiveJob);
 
 module.exports = router;
